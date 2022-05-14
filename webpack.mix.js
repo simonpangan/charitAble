51aefbce20ea8a719d9074d656030ab141793058
Simon Pangan <simonjoseph.pangan@gmail.com>
2022-05-15 02:10:30 +0800

    Initial Commit

---
+++ webpack.mix.js
@@ -0,0 +1,21 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+ mix.vue(3)
+	.js('resources/js/app.js', 'public/js')
+	.js('resources/js/inertia-vue.js', 'public/js')
+	.js('resources/js/bootstrap.js', 'public/js')
+	.sass('resources/sass/app.scss', 'public/css')
+	.sass('resources/sass/bootstrap.scss', 'public/css')
+	.sourceMaps()
+	.version(); 
