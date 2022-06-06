e31bbc3b0be7627f3dff96dd67430dac32a3f54c
kevinxu101 <55251053+kevinxu101@users.noreply.github.com>
2022-06-06 23:16:09 +0800

    UI Changes on Verify Email
    Outside of mail trap verify email doesn't seem to work.

--- resources/js/Pages/Auth/Verify.vue
+++ resources/js/Pages/Auth/Verify.vue
@@ -1,23 +1,38 @@
 <template>
-    <h2>EMAIL VERIFICATION</h2>
-    <div class="card-body">
-        <div v-if="$page.props.flash.resent" class="alert alert-success" role="alert">
-            A fresh verification link has been sent to your email address.
+  <div class="bg-white">
+    <div class="container">
+      <div class="row justify-content-center align-items-center d-flex vh-100">
+        <div class="col-md-4 mx-auto">
+          <div class="osahan-login py-4">
+            <div class="text-center mb-4">
+              <a href="index.html"><img src="img/logo.svg" alt="" /></a>
+              <h5 class="font-weight-bold mt-3">We're almost there!</h5>
+              <p class="text-muted">Please check your email inbox.</p>
+            </div>
+            <div
+              v-if="$page.props.flash.resent"
+              class="alert alert-success"
+              role="alert"
+            ></div>
+            Before proceeding, please check your email for a verification link
+            <br />
+            If you did not receive the email
+            <form class="d-inline" @submit.prevent="submit">
+              <button type="submit" class="btn btn-link p-0 m-0 align-baseline">
+                click here to request another
+              </button>
+            </form>
+          </div>
         </div>
-
-        Before proceeding, please check your email for a verification link
-        <br/>
-        If you did not receive the email
-        <form class="d-inline" @submit.prevent="submit">
-            <button type="submit" class="btn btn-link p-0 m-0 align-baseline">click here to request another</button>.
-        </form>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
-import { Inertia } from '@inertiajs/inertia';
+import { Inertia } from "@inertiajs/inertia";
 
 let submit = () => {
-    Inertia.post(route('auth.verification.send'));
-}
+  Inertia.post(route("auth.verification.send"));
+};
 </script>
