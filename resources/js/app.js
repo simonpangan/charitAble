ed5ec95c03e44215e4ddbb0a67eab4ab1f5c8e2b
Simon <simojoseph.pangan@gmail.com>
2022-06-10 18:28:45 +0800

    Add progress indicicator

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
+import { InertiaProgress } from '@inertiajs/progress'
 
 createInertiaApp({
     resolve: name => require(`./Pages/${name}`),
@@ -13,4 +14,8 @@ createInertiaApp({
 
         return VueApp;
     },
-})
+});
+
+InertiaProgress.init({
+    showSpinner: true,
+});
