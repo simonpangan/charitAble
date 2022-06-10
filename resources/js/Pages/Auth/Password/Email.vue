075f8b88ffd2be045b0f9265397378542595e3f3
Simon <simojoseph.pangan@gmail.com>
2022-06-10 20:33:53 +0800

    update email vue

--- resources/js/Pages/Auth/Password/Email.vue
+++ resources/js/Pages/Auth/Password/Email.vue
@@ -2,38 +2,38 @@
   <div class="bg-white">
     <div class="container">
       <div class="row justify-content-center align-items-center d-flex vh-100">
-        <div class="col-md-8 mx-auto">
-          <div class="osahan-login py-4">
+        <div class="col-md-4 mx-auto">
+          <div class="py-4">
             <div class="text-center mb-4">
-              <a href="index.html"><img src="img/logo.svg" alt="" /></a>
+              <a href="index.html">
+                <img src="img/logo.svg" alt="charitAble logo" />
+              </a>
               <h5 class="font-weight-bold">First, let's find your account</h5>
               <p class="text-muted">Please enter your email address</p>
             </div>
             <form @submit.prevent="submit">
               <div v-if="$page.props.flash.status" v-text="$page.props.flash.status" 
-                class="alert alert-success w-70 mx-auto text-center" role="alert">
+                class="alert alert-success w-80 mx-auto text-center" role="alert">
               </div>
-              <div class="row mb-3">
-                <label for="email" class="col-md-4 col-form-label text-md-end">
-                  Email Address
-                </label>
-                <div class="col-md-6">
+              <div class="mb-3">
+                <label class="mb-1">Email or Phone</label>
+                <div class="position-relative icon-form-control">
+                  <i class="feather-user position-absolute"></i>
                   <input id="email" type="email" v-model="form.email" 
                     :class="{ 'is-invalid': form.errors.email }" 
-                    class="form-control"  required autocomplete="email"
-                    autofocus
+                    class="form-control" 
                   />
-                  <span v-if="form.errors.email" class="invalid-feedback" role="alert" >
-                    <strong v-text="form.errors.email"></strong>
-                  </span>
                 </div>
+                <span v-if="form.errors.email" class="invalid-feedback d-block" role="alert">
+                  <strong v-text="form.errors.email"></strong>
+                </span>
               </div>
-              <div class="row mb-0">
-                <div class="col-md-6 offset-md-4">
-                  <button type="submit" :disabled="form.processing" class="btn btn-primary">
-                    Send Password Reset Link
-                  </button>
-                </div>
+              <button type="submit" :disabled="form.processing" class="btn btn-primary d-block w-100 text-uppercase"> Send Password Reset Link </button>
+              <div class="py-3 d-flex align-item-center">
+                <Link :href="$route('auth.index')">Sign In</Link>
+                <span class="ms-auto"> New to Osahanin? 
+                  <a class="fw-bold" href="sign-up.html">Join now</a>
+                </span>
               </div>
             </form>
           </div>
@@ -42,15 +42,16 @@
     </div>
   </div>
 </template>
-
 <script setup>
-import { useForm } from "@inertiajs/inertia-vue3";
+  import {
+    useForm, Link
+  } from "@inertiajs/inertia-vue3";
 
-let form = useForm({
-  email: "",
-});
+  let form = useForm({
+    email: "",
+  });
 
-let submit = () => {
-  form.post(route("auth.password.email"));
-};
+  let submit = () => {
+    form.post(route("auth.password.email"));
+  };
 </script>
