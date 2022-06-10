dc974406778c24e054729aa34471c614ac6fdb58
Simon <simojoseph.pangan@gmail.com>
2022-06-10 13:10:29 +0800

    Error Pages

---
+++ resources/js/Pages/Errors/Error.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Forbidden v-if="status === 403" />
+    <NotFound v-else-if="status === 404" />
+    <Maintenance v-else-if="status === 503" />
+  </div>
+</template>
+
+<script>
+import Forbidden from "./Forbidden.vue";
+import Maintenance from "./Maintenance.vue";
+import NotFound from "./NotFound.vue";
+
+export default {
+  props: {
+    status: Number,
+  },
+  components: {
+    Forbidden,
+    Maintenance,
+    NotFound,
+  },
+};
+</script>
