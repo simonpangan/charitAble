67caedbcfa9405e410a4c84b9a5b716b659377ca
Simon <simojoseph.pangan@gmail.com>
2022-06-10 18:14:47 +0800

    Add success message for email request

--- resources/js/Pages/Auth/Password/Email.vue
+++ resources/js/Pages/Auth/Password/Email.vue
@@ -10,37 +10,27 @@
               <p class="text-muted">Please enter your email address</p>
             </div>
             <form @submit.prevent="submit">
+              <div v-if="$page.props.flash.status" v-text="$page.props.flash.status" 
+                class="alert alert-success w-70 mx-auto text-center" role="alert">
+              </div>
               <div class="row mb-3">
-                <label for="email" class="col-md-4 col-form-label text-md-end"
-                  >Email Address</label
-                >
+                <label for="email" class="col-md-4 col-form-label text-md-end">
+                  Email Address
+                </label>
                 <div class="col-md-6">
-                  <input
-                    id="email"
-                    type="email"
-                    :class="{ 'is-invalid': form.errors.email }"
-                    class="form-control"
-                    v-model="form.email"
-                    required
-                    autocomplete="email"
+                  <input id="email" type="email" v-model="form.email" 
+                    :class="{ 'is-invalid': form.errors.email }" 
+                    class="form-control"  required autocomplete="email"
                     autofocus
                   />
-                  <span
-                    v-if="form.errors.email"
-                    class="invalid-feedback"
-                    role="alert"
-                  >
+                  <span v-if="form.errors.email" class="invalid-feedback" role="alert" >
                     <strong v-text="form.errors.email"></strong>
                   </span>
                 </div>
               </div>
               <div class="row mb-0">
                 <div class="col-md-6 offset-md-4">
-                  <button
-                    type="submit"
-                    :disabled="form.processing"
-                    class="btn btn-primary"
-                  >
+                  <button type="submit" :disabled="form.processing" class="btn btn-primary">
                     Send Password Reset Link
                   </button>
                 </div>
