724f308e75d4da3a1d79f1309e64cd53e2e39ce4
Simon <simojoseph.pangan@gmail.com>
2022-06-11 18:50:17 +0800

    Add validation unit test for charity volunteer posts

---
+++ tests/Unit/Validations/CharityVolunterPostUnitTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Requests\CharityVolunteerPostRequest;
+use App\Http\Controllers\Charity\CharityVolunteerPostController;
+
+
+
+it('should use BenefactorRegisterRequest class', function () {
+    $this->assertActionUsesFormRequest(
+        CharityVolunteerPostController::class,
+        'store',
+        CharityVolunteerPostRequest::class
+    );
+
+    $this->assertActionUsesFormRequest(
+        CharityVolunteerPostController::class,
+        'update',
+        CharityVolunteerPostRequest::class
+    );
+});
+
+it('should have exact validation', function () {
+    $request = new CharityVolunteerPostRequest();
+
+    $this->assertExactValidationRules([
+        'description' => ['required', 'string'],
+        'location' => ['required', 'string'],
+    ], $request->rules());
+});
