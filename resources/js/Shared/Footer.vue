d1af52691f2230e16dbd76c2ed4632a6dd5b69b1
Simon <simojoseph.pangan@gmail.com>
2022-06-12 17:55:46 +0800

    update footer

--- resources/js/Shared/Footer.vue
+++ resources/js/Shared/Footer.vue
@@ -1,30 +1,31 @@
 <template>
-	Footer
-	Footer
-	Footer
-	Footer
-	Footer
-	Footer
-	Footer
-	Footer
-	Footer
-	Footer
-	Footer
-	Footer
-	Footer
-	Footer
-	Footer
-	Footer
-	Footer
-	Footer
-	Footer
-	Footer
-	Footer
-	Footer
-	Footer
-	Footer
-	Footer
-	Footer
-	Footer
-	Footer
+  <footer class="bg-light py-4 fixed-bottom">
+    <div class="container">
+      <div class="d-flex justify-content-between align-items-center">
+        <p class="small text-muted mb-0">© charitAble 2022</p>
+        <ul class="list-inline mb-0">
+          <li class="list-inline-item">
+            <a class="btn btn-sm btn-light" href="#">
+              <span class="feather-facebook"></span>
+            </a>
+          </li>
+          <li class="list-inline-item">
+            <a class="btn btn-sm btn-light" href="#">
+              <span class="feather-youtube"></span>
+            </a>
+          </li>
+          <li class="list-inline-item">
+            <a class="btn btn-sm btn-light" href="#">
+              <span class="feather-twitter"></span>
+            </a>
+          </li>
+          <li class="list-inline-item">
+            <a class="btn btn-sm btn-light" href="#">
+              <span class="feather-github"></span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </footer>
 </template>
