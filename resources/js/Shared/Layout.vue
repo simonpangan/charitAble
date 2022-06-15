bb602d21d5ef9891890558a8863708e91c49186e
Simon <simojoseph.pangan@gmail.com>
2022-06-15 22:13:44 +0800

    change layout to composition api

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -1,11 +1,7 @@
 <template>
-    <!-- Hello, {{ username }} -->  
     <Head>
         <title>My app</title>
-        <meta type="description" 
-            content="Information About my app" 
-            head-key="description"
-        >
+        <meta type="description" content="Information About my app" head-key="description">
         <!-- <link rel="icon" type="image/svg+xml" href="/favicon.svg" /> -->
     </Head>
 
@@ -18,18 +14,8 @@
     <Footer />
 </template>
 
-<script>
+<script setup>
 import Nav from "./Nav";
 import Footer from "./Footer";
 import { Head } from "@inertiajs/inertia-vue3";
-
-export default {
-    name: "Layout",
-    components: {Nav, Head, Footer},
-    // computed: {
-    //     username() {
-    //         return this.$page.props.auth.user.username;
-    //     }
-    // }
-}
 </script>
