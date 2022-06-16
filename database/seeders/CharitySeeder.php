0e60fb4bf57f8aacbef4c7e8373e0aa9ccd91e19
Simon <simojoseph.pangan@gmail.com>
2022-06-16 20:25:06 +0800

    update seeder

--- database/seeders/CharitySeeder.php
+++ database/seeders/CharitySeeder.php
@@ -2,11 +2,15 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
+use App\Models\Benefactor;
 use App\Enums\CharityCategory;
+use App\Models\Charity\Charity;
 use Illuminate\Database\Seeder;
-use App\Models\Charity\CharityCategories;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Support\Facades\DB;
+use App\Models\Charity\CharityCategories;
+use App\Models\Charity\CharityFollowers;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class CharitySeeder extends Seeder
 {
@@ -28,6 +32,27 @@ class CharitySeeder extends Seeder
             DB::table('categories')
                 ->insert($categories);
         }
-       
+
+        $userFollowing = array();
+        $benefactorID = User::where('email', 'simonpangan@yahoo.com')->first(['id'])->id;
+
+        if (Charity::count() < 1000) 
+        {
+            $users = User::factory()->count(10)->charitySuperAdmin()->create();
+
+            $users->each(function ($user) use ($benefactorID, &$userFollowing) {
+                Charity::factory()
+                    ->create([
+                        'id' => $user->id
+                    ]);
+                    
+                array_push($userFollowing, [
+                    'benefactor_id' => $benefactorID,
+                    'charity_id' => $user->id,
+                ]);
+            });
+        }
+        
+        CharityFollowers::insert($userFollowing);
     }
 }
--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -12,7 +12,6 @@ use App\Models\Charity\Charity;
 use Illuminate\Database\Seeder;
 use Illuminate\Container\Container;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\LazyCollection;
 
 class UsersSeeder extends Seeder
@@ -24,42 +23,55 @@ class UsersSeeder extends Seeder
      */
     public function run()
     {
+//        LazyCollection::make(function () {
+//            for ($i = 0; $i < 50000; $i++) {
+//                yield User::factory()->raw();
+//            }
+//        })->chunk(5000)->each(function ($chunk) {
+//            User::insert($chunk->toArray());
+//        });
+
+//        $users = collect(User::factory()->count(10)->unverified()->benefactor()->raw());
+//        $users = User::factory()->count(50000)->lazyRaw();
+//
+//        $users->chunk(5000)->each(function ($chunk) {
+//            User::insertOrIgnore($chunk->toArray());
+//        });
+
         $benefactor = User::firstOrCreate(
-            ['email' => 'simonpangan@yahoo.com'],
             [
-            'role_id' => Role::USERS['BENEFACTOR'],
+                'email' => 'simonpangan@yahoo.com',
+                'role_id' => Role::USERS['BENEFACTOR'],
                'email_verified_at' => Carbon::now(config('app.timezone')),
-            // 'email_verified_at' => null,
-            'password' => Hash::make('simonpangan'),
+                // 'email_verified_at' => null,    
+                'password' => Hash::make('simonpangan'),
             ]
         );
         
+        $benefactor->logs()->createMany($this->createLogsForUser());
 
-        $benefactor->created(function () use ($benefactor) {
-            $benefactor->logs()->createMany($this->createLogsForUser());
-
-            $benefactor->benefactor()->create([
-                'first_name' => 'Simon Joseph',
-                'last_name' => 'Pangan',
-                'gender' => 'Male',
-                'age' => '22',
-                'city' => 'Valenzuela',
-                'preferences' => ['COMMUNITY_DEVELOPMENT'],
-                'account_type' => 'Personal',
-                'total_donation' => '10000',
-                'total_charities_donated' => '10',
-                'total_charities_followed' => '5',
-                'total_number_donations' => '10',
-            ]);
-        });
+        $benefactor->benefactor()->create([
+            'first_name' => 'Simon Joseph',
+            'last_name' => 'Pangan',
+            'gender' => 'Male',
+            'age' => '22',
+            'city' => 'Valenzuela',
+            'preferences' => ['COMMUNITY_DEVELOPMENT'],
+            'account_type' => 'Personal',
+            'total_donation' => '10000',
+            'total_charities_donated' => '10',
+            'total_charities_followed' => '5',
+            'total_number_donations' => '10',
+        ]);
 
         $charity = User::firstOrCreate(
             ['email' => 'charity@gmail.com'],
             [
-            'role_id' => Role::USERS['CHARITY_SUPER_ADMIN'],
-            'email_verified_at' => Carbon::now(config('app.timezone')),
-            'password' => Hash::make('charity'),
-        ]
+                'email' => 'charity@gmail.com',
+                'role_id' => Role::USERS['CHARITY_SUPER_ADMIN'],
+                'email_verified_at' => Carbon::now(config('app.timezone')),
+                'password' => Hash::make('charity'),
+            ]
         );
 
         // if (Log::where('user_id', $charity->id)->get()->count() < 5) {
@@ -67,14 +79,14 @@ class UsersSeeder extends Seeder
         //     $charity->logs()->createMany($this->createLogsForUser());
         // }
 
-        if (User::count() < 10000) {
-            // $users = collect(User::factory()->count(10)->unverified()->benefactor()->raw());
-            $users = User::factory()->count(50000)->lazyRaw();
+        // if (User::count() < 10000) {
+        //     // $users = collect(User::factory()->count(10)->unverified()->benefactor()->raw());
+        //     $users = User::factory()->count(50000)->lazyRaw();
 
-            $users->chunk(5000)->each(function ($chunk) {
-                User::insertOrIgnore($chunk->toArray());
-            });
-        }
+        //     $users->chunk(5000)->each(function ($chunk) {
+        //         User::insertOrIgnore($chunk->toArray());
+        //     });
+        // }
     }
 
     private function createLogsForUser(): array
