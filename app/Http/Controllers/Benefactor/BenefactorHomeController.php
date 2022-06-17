d5e9a039fa50f2def2adde48d9bdc465d49cb747
kevinxu101 <kevin.xu@benilde.edu.ph>
2022-06-17 16:28:59 +0800

    Initial Post algorithm, single ID only.

--- app/Http/Controllers/Benefactor/BenefactorHomeController.php
+++ app/Http/Controllers/Benefactor/BenefactorHomeController.php
@@ -4,11 +4,32 @@ namespace App\Http\Controllers\Benefactor;
 
 use Inertia\Inertia;
 use Inertia\Response;
+use App\Models\Charity\CharityPosts;
+use App\Models\Charity\CharityVolunteerPost;
+use Illuminate\Support\Facades\Auth;
+
 
 class BenefactorHomeController
 {
     public function index(): Response
     {
-        return Inertia::render('Benefactor/Home');
+
+        //Initial Algorithm
+        /*
+            'posts' => CharityPosts::all()
+                        ->where(charity_followers::where('benefactor_id',auth->user-id()))
+                        ->orderBy('created_at', 'DESC')
+                        ->paginate(?)
+
+
+
+                        
+        */
+        return Inertia::render('Benefactor/Home',[
+            'user' => Auth::user()->withBenefactor()->toArray(),
+            'posts' => CharityPosts::where('charity_id',52)->get()->toArray(),
+            'volunteer_post'=> CharityVolunteerPost::where('charity_id',52)->get()->toArray()
+
+        ]);
     }
 }
