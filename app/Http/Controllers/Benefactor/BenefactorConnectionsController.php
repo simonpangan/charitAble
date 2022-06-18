d59c09147a931258cdf7e92aff2ea32a970ddbc1
Simon <simojoseph.pangan@gmail.com>
2022-06-18 18:41:33 +0800

    add search

--- app/Http/Controllers/Benefactor/BenefactorConnectionsController.php
+++ app/Http/Controllers/Benefactor/BenefactorConnectionsController.php
@@ -4,19 +4,32 @@ namespace App\Http\Controllers\Benefactor;
 
 use Inertia\Inertia;
 use Inertia\Response;
-use App\Enums\CharityCategory;
 use App\Models\Benefactor;
+use Illuminate\Http\Request;
+use App\Enums\CharityCategory;
 use App\Models\Charity\CharityFollowers;
 
 class BenefactorConnectionsController
 {
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $charityFollowingLists = Benefactor::auth()
-            ->followingCharities()
-            ->get(['name']);
-        
-        $charityFollowingCategotyNumber =  CharityFollowers::query()
+        return Inertia::render('Benefactor/Connections/Index', [
+            'charityCategories' => CharityCategory::getCategoriesValues(),
+            'followingList' => $this->getCharityFollowingLists($request->get('search')),
+            'charityFollowingCategotyNumber' => fn () => $this->getFharityFollowingPerCategotyStats(),
+        ]);
+    }
+
+    private function getCharityFollowingLists(string $name = null)
+    {
+        return Benefactor::auth()
+            ->followingCharitiesByName($name)
+            ->get(['name', 'id']);
+    }
+
+    private function getFharityFollowingPerCategotyStats()
+    {
+        return CharityFollowers::query()
             ->join('charities', 'charities.id', '=', 'charity_followers.charity_id')
             // ->selectRaw("count(case when status = 'confirmed' then 1 end) as confirmed")
             // ->selectRaw("count(case when status = 'unconfirmed' then 1 end) as unconfirmed")
@@ -24,13 +37,5 @@ class BenefactorConnectionsController
             // ->selectRaw("count(case when status = 'bounced' then 1 end) as bounced")
             ->selectRaw('count(*) as total')
             ->first();
-
-        
-
-        return Inertia::render('Benefactor/Connections', [
-            'charityCategories' => CharityCategory::getCategoriesValues(),
-            'followingList' => $charityFollowingLists,
-            'charityFollowingCategotyNumber' => $charityFollowingCategotyNumber,
-        ]);
     }
 }
