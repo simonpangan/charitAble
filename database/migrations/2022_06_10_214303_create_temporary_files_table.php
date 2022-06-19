aafd7aa11d9c43a707a80ac8e80253ea512de90a
kevinxu101 <kevin.xu@benilde.edu.ph>
2022-06-20 01:10:49 +0800

    Added File Type field

--- database/migrations/2022_06_10_214303_create_temporary_files_table.php
+++ database/migrations/2022_06_10_214303_create_temporary_files_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->id();
             $table->string('folder');
             $table->string('filename');
+            $table->string('file_type');
             $table->timestamps();
         });
     }
