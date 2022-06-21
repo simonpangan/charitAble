a4ba986aac6acd9c13e52f3782aaf6f168486e46
Simon <simojoseph.pangan@gmail.com>
2022-06-21 20:06:12 +0800

    update footer css

--- resources/js/Shared/Footer.vue
+++ resources/js/Shared/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <footer class="footer mt-auto d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+    <footer class="footer mt-auto d-flex flex-wrap justify-content-between align-items-center border-top">
       <p class="col-md-4 mb-0 text-muted">© 2022 charitAble</p>
       <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
         Logo
