003ec61581424ace280e7fa1014b2162e530b3de
Simon <simojoseph.pangan@gmail.com>
2022-06-23 14:44:29 +0800

    add seeder for program donation

---
+++ database/seeders/ProgramDonationSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Program;
+use App\Models\Benefactor;
+use App\Models\Charity\CharityProgram;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class ProgramDonationSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+        $benefactor = Benefactor::find(2);
+
+
+        $program = CharityProgram::all();
+
+        foreach ($program->random(20) as $program) {
+            $benefactor->programDonations()->attach($program->id, [
+                'amount' => 100 
+            ]);
+        }
+    }
+}
