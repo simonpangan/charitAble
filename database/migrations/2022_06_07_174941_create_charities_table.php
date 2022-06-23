e89f1100f4eb4074225d062ce53892cf3f3a6237
Simon <simojoseph.pangan@gmail.com>
2022-06-23 21:04:34 +0800

    change followers column to int

--- database/migrations/2022_06_07_174941_create_charities_table.php
+++ database/migrations/2022_06_07_174941_create_charities_table.php
@@ -30,7 +30,7 @@ return new class extends Migration
             $table->boolean('is_pnc_accredited')->nullable();
             $table->timestamp('charity_verified_at')->nullable();
 
-            $table->string('followers')->nullable();
+            $table->integer('followers');
             
             $table->timestamps();
         });
