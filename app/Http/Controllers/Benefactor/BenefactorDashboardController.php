438be783bd243cbfe3c70e473157a9a8f1d34c58
Simon <simojoseph.pangan@gmail.com>
2022-06-23 18:42:21 +0800

    updated dashboard logic

--- app/Http/Controllers/Benefactor/BenefactorDashboardController.php
+++ app/Http/Controllers/Benefactor/BenefactorDashboardController.php
@@ -4,22 +4,40 @@ namespace App\Http\Controllers\Benefactor;
 
 use Inertia\Inertia;
 use Inertia\Response;
-use App\Http\Controllers\Controller;
 use App\Models\Benefactor;
+use App\Models\Charity\Charity;
+use App\Http\Controllers\Controller;
 
 class BenefactorDashboardController extends Controller
 {
     public function index(): Response
     {
-        $benefactor = Benefactor::auth();
-
-        $programDonations = $benefactor->programDonations()
-            ->latest()
-            ->paginate(10, ['name', 'program_donations.amount']);
-
         return Inertia::render('Benefactor/Dashboard', [
-            'benefactor' => $benefactor,    
-            'programDonations' => $programDonations 
+            'benefactor' => fn() => $this->getBenefactorAuth(),
+            'programDonations'=> fn() => $this->getProgramDonations(),
+            'charities' => fn () => $this->getCharities()
         ]);
+    }
+
+    private function getBenefactorAuth()
+    {
+        return Benefactor::auth();
+    }
+
+    private function getProgramDonations()
+    {
+        return $this->getBenefactorAuth()->programDonations()
+        ->latest()
+        ->paginate(10, ['name', 'program_donations.amount']);
+    }
+
+    private function getCharities()
+    {
+        $charityID = $this->getBenefactorAuth()->programDonations()
+            ->latest('donated_at')
+            ->get(['name', 'charity_id','program_donations.amount'])
+            ->pluck('charity_id')->unique()->toArray();
+
+        return Charity::whereIn('id', $charityID)->paginate(10);
     }
 }
--- resources/js/Pages/Benefactor/Dashboard.vue
+++ resources/js/Pages/Benefactor/Dashboard.vue
@@ -92,129 +92,154 @@
         </div>
       </div>
     </div>
-  <div class="row">
-            <div class="col-md-8">
-              <div class="card">
-                <div class="card-header">
-                  <h4>Invoices</h4>
-                  <div class="card-header-action">
-                    <a href="#" class="btn btn-danger">View More <i class="fas fa-chevron-right"></i></a>
-                  </div>
-                </div>
-                <div class="card-body p-0">
-                  <div class="table-responsive table-invoice">
-                    <table class="table table-striped">
-                      <tbody><tr>
-                        <th>Invoice ID</th>
-                        <th>Customer</th>
-                        <th>Status</th>
-                        <th>Due Date</th>
-                        <th>Action</th>
-                      </tr>
-                      <tr>
-                        <td><a href="#">INV-87239</a></td>
-                        <td class="font-weight-600">Kusnadi</td>
-                        <td><div class="badge badge-warning">Unpaid</div></td>
-                        <td>July 19, 2018</td>
-                        <td>
-                          <a href="#" class="btn btn-primary">Detail</a>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td><a href="#">INV-48574</a></td>
-                        <td class="font-weight-600">Hasan Basri</td>
-                        <td><div class="badge badge-success">Paid</div></td>
-                        <td>July 21, 2018</td>
-                        <td>
-                          <a href="#" class="btn btn-primary">Detail</a>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td><a href="#">INV-76824</a></td>
-                        <td class="font-weight-600">Muhamad Nuruzzaki</td>
-                        <td><div class="badge badge-warning">Unpaid</div></td>
-                        <td>July 22, 2018</td>
-                        <td>
-                          <a href="#" class="btn btn-primary">Detail</a>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td><a href="#">INV-84990</a></td>
-                        <td class="font-weight-600">Agung Ardiansyah</td>
-                        <td><div class="badge badge-warning">Unpaid</div></td>
-                        <td>July 22, 2018</td>
-                        <td>
-                          <a href="#" class="btn btn-primary">Detail</a>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td><a href="#">INV-87320</a></td>
-                        <td class="font-weight-600">Ardian Rahardiansyah</td>
-                        <td><div class="badge badge-success">Paid</div></td>
-                        <td>July 28, 2018</td>
-                        <td>
-                          <a href="#" class="btn btn-primary">Detail</a>
-                        </td>
-                      </tr>
-                    </tbody></table>
-                  </div>
-                </div>
+    <br />
+    <br />
+    <br />
+    <br />
+    <div class="container mt-n10">
+      <div class="row">
+        <div class="col">
+          <div class="card mb-4">
+          <div class="card-header h4">Your Donations History</div>
+          <div class="card-body">
+            <div class="dataTable-wrapper dataTable-loading no-footer sortable searchable fixed-columns">
+              <div class="dataTable-top">
               </div>
-            </div>
-            <div class="col-md-4">
-              <div class="card card-hero">
-                <div class="card-header">
-                  <div class="card-icon">
-                    <i class="far fa-question-circle"></i>
-                  </div>
-                  <h4>14</h4>
-                  <div class="card-description">Customers need help</div>
+              <div class="dataTable-container">
+                <card id="datatablesSimple" class="dataTable-table">
+                  <thead>
+                    <tr style="width: 100%">
+                      <th style="width: 10%;" class="text-center">
+                        #
+                      </th>
+                      <th style="width: 20%;" class="text-center">
+                        Program Name
+                      </th>
+                      <th style="width: 20%;" class="text-center">
+                        Amount
+                      </th>
+                      <th style="width: 30%;" class="text-center">
+                        Donated At 
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+     
+                    <tr v-for="(donation, index) in programDonations.data" :key="index">
+                      <td class="text-center">{{ programDonations.from + index }}</td>
+                      <td>{{ donation.name }}</td>
+                      <td>{{ donation.amount }}</td>
+                      <td>{{ donation.pivot.donated_at_formatted }}</td>
+                    </tr>
+                    <tr v-if="programDonations.data.length == 0">
+                      <td class="dataTables-empty" colspan="4" width="1300">
+                        No results
+                      </td>
+                    </tr>
+                  </tbody>
+                </card>
+              </div>
+              <div class="dataTable-bottom mt-3">
+                <div class="dataTable-info">
+                  Showing {{ programDonations.from }} to {{ programDonations.to }} 
+                  of {{ programDonations.total }} entries
                 </div>
-                <div class="card-body p-0">
-                  <div class="tickets-list">
-                    <a href="#" class="ticket-item">
-                      <div class="ticket-title">
-                        <h4>My order hasn't arrived yet</h4>
-                      </div>
-                      <div class="ticket-info">
-                        <div>Laila Tazkiah</div>
-                        <div class="bullet"></div>
-                        <div class="text-primary">1 min ago</div>
-                      </div>
-                    </a>
-                    <a href="#" class="ticket-item">
-                      <div class="ticket-title">
-                        <h4>Please cancel my order</h4>
-                      </div>
-                      <div class="ticket-info">
-                        <div>Rizal Fakhri</div>
-                        <div class="bullet"></div>
-                        <div>2 hours ago</div>
-                      </div>
-                    </a>
-                    <a href="#" class="ticket-item">
-                      <div class="ticket-title">
-                        <h4>Do you see my mother?</h4>
-                      </div>
-                      <div class="ticket-info">
-                        <div>Syahdan Ubaidillah</div>
-                        <div class="bullet"></div>
-                        <div>6 hours ago</div>
-                      </div>
-                    </a>
-                    <a href="features-tickets.html" class="ticket-item ticket-more">
-                      View All <i class="fas fa-chevron-right"></i>
-                    </a>
-                  </div>
-                </div>
+                <nav class="dataTable-pagination">
+                  <ul class="dataTable-pagination-list">
+                    <li v-for="(link, index) in programDonations.links" :key=index 
+                      :class="[
+                        link.active ? 'active' : '',
+                        link.url ? 'pager' : '',
+                    ]"> 
+                      <Component 
+                        :is="link.url ? 'Link' : 'span'"
+                        v-if="link.url" 
+                        :href="link.url" 
+                        v-html="link.label"
+                        :only="['programDonations']"
+                      >
+                      </Component>
+                    </li>
+                  </ul>
+                </nav>
               </div>
             </div>
           </div>
+        </div>
+        </div>
+        <div class="col">
+          <div class="card mb-4">
+          <div class="card-header h4">Charities You have donated</div>
+          <div class="card-body">
+            <div class="dataTable-wrapper dataTable-loading no-footer sortable searchable fixed-columns">
+              <div class="dataTable-top">
+              </div>
+              <div class="dataTable-container">
+                <card id="datatablesSimple" class="dataTable-table">
+                  <thead>
+                    <tr style="width: 100%">
+                      <th style="width: 10%;" class="text-center">
+                        #
+                      </th>
+                      <th style="width: 20%;" class="text-center">
+                        Charity Name
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="(charity, index) in charities.data" :key="index">
+                      <td class="text-center">{{ charities.from + index }}</td>
+                      <td>{{ charity.name }}</td>
+                    </tr>
+                    <tr v-if="charities.data.length == 0">
+                      <td class="dataTables-empty" colspan="3" width="1300">
+                        No results match your search query
+                      </td>
+                    </tr>
+                  </tbody>
+                </card>
+              </div>
+              <div class="dataTable-bottom mt-3">
+                <div class="dataTable-info">
+                  Showing {{ charities.from }} to {{ charities.to }} 
+                  of {{ charities.total }} entries
+                </div>
+                <nav class="dataTable-pagination">
+                  <ul class="dataTable-pagination-list">
+                    <li v-for="(link, index) in charities.links" :key=index 
+                      :class="[
+                        link.active ? 'active' : '',
+                        link.url ? 'pager' : '',
+                    ]"> 
+                      <Component 
+                        :is="link.url ? 'Link' : 'span'"
+                        v-if="link.url" 
+                        :href="link.url" 
+                        v-html="link.label"
+                        :only="['charities']"
+                      >
+                      </Component>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
 <script setup>
   let props = defineProps({
-    auth: Object,
-    benefactor: Object
+    benefactor: Object,
+    programDonations: Object,
+    charities: Array
   });
+</script>
+
+<script> 
+  import  '../../../../public/css/datatable.css'; 
 </script>
