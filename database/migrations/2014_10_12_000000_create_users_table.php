68576773f484aed2c42b6da2245ff6f57220606c
Simon <simojoseph.pangan@gmail.com>
2022-06-25 00:38:48 +0800

    add last generate date time column in users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
             $table->rememberToken()->nullable();
+            $table->dateTime('last_generate_report')->nullable();
             $table->timestamps();
         });
     }
