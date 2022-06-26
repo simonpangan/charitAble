cb9682993869a93712c073d380a79d0633f7d995
Simon <simojoseph.pangan@gmail.com>
2022-06-26 14:31:04 +0800

    update navbar and layout

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -4,20 +4,7 @@
       <a class="navbar-brand me-2" href="/">
         Logo
       </a>
-      <form class="d-none d-sm-inline-block form-inline me-auto my-2 my-md-0 mw-100 navbar-search">
-        <div class="input-group">
-            <input v-model="form.seach" type="text" class="form-control shadow-none border-0 text-white" 
-              placeholder="Search peopse, jobs & more..." 
-              aria-label="Search" 
-              aria-describedby="basic-addon2"
-            />
-            <div class="input-group-append">
-              <button class="btn h-100" type="submit">
-                <i class="feather-search"></i>
-              </button>
-            </div>
-        </div>
-      </form>
+   
       <ul v-if="auth === undefined" class="navbar-nav ms-auto d-flex align-items-center">
         <li class="nav-item">
           <NavLink class="nav-link" href="/login">
@@ -28,6 +15,7 @@
       </ul>
       <ul v-else class="navbar-nav ms-auto d-flex align-items-center">
         <BenefactorNav v-if="role == 'BENEFACTOR'"/>
+        <AdminNav v-if="role == 'ADMIN'"/>
         <CharityNav v-if="role == 'CHARITY_SUPER_ADMIN' || role == 'CHARITY_ADMIN'"/>
       </ul>
     </div>
@@ -37,6 +25,7 @@
 <script setup>
 import NavLink from "./Nav/NavLink";
 import BenefactorNav from "./Nav/BenefactorNav";
+import AdminNav from "./Nav/AdminNav";
 import CharityNav from "./Nav/CharityNav";
 import { computed } from 'vue';
 import { Link, useForm } from '@inertiajs/inertia-vue3';
---
+++ resources/js/Shared/Nav/AdminNav.vue
@@ -0,0 +1,21 @@
+<template>
+	<li class="nav-item">
+	<NavLink class="nav-link btn btn-link" href="/benefactor/home">
+		<span class="d-none d-lg-inline">Charities</span>
+	</NavLink>
+	</li>
+	<li class="nav-item">
+	<NavLink class="nav-link btn btn-link" href="/benefactor/charity-search/followers">
+		<span class="d-none d-lg-inline">Withdrawal</span>
+	</NavLink>
+	</li>
+	<li class="nav-item">
+	<NavLink class="nav-link btn btn-link" href="/logout" method="post" as="button">
+		Logout
+	</NavLink>
+	</li>
+</template>
+
+<script setup>
+import NavLink from "./NavLink";
+</script>
