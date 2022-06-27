5baa20b4a6fc236c727367afc34cd26c1061573f
Simon <simojoseph.pangan@gmail.com>
2022-06-27 21:28:41 +0800

    update contract deployment

--- blockchain/charitable.js
+++ blockchain/charitable.js
@@ -1,8 +1,6 @@
 import web3 from "./web3";
 const { abi } = require("./build/contracts/CharitAble.json");
 
-const charitableContract = (address) => {
-    return new web3.eth.Contract(abi, address);
-};
+const charitableContract = new web3.eth.Contract(abi, '0xFaAe17Ac63D320903C4bC8600e45cB721D70722c');
 
 export default charitableContract;
--- blockchain/web3.js
+++ blockchain/web3.js
@@ -1,18 +1,15 @@
-require('dotenv').config();
-const { INFURA_API_URL } = process.env;
-
-import Web3 from "web3";
+import Web3 from 'web3/dist/web3.min.js';
 
 let web3;
 
 if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
     // we are in the browser && metamask is running
     web3 = new Web3(window.ethereum);
-    window.ethereum.request({ method: "eth_requestAccounts" });
+    // window.ethereum.request({ method: "eth_requestAccounts" });
 } else {
     // we are on the server OR the user is not running metamask     
     const provider = new Web3.providers
-        .HttpProvider(INFURA_API_URL);
+        .HttpProvider("https://rinkeby.infura.io/v3/7e4a83640a004d40be88e9e184c3ee9f");
     web3 = new Web3(provider);
 }
 
