a4c5078f77de6ba4f4920f2fb53e3bc1623b78bb
Simon <simojoseph.pangan@gmail.com>
2022-06-29 00:27:19 +0800

    update charity nav icons

--- resources/js/Shared/Nav/CharityNav.vue
+++ resources/js/Shared/Nav/CharityNav.vue
@@ -2,23 +2,22 @@
 	<li class="nav-item">
 		<NavLink class="nav-link btn btn-link" href="#" 
 			method="post" as="button">
-			<i class="feather-users me-2"></i>
+			<!-- <i class="feather-users me-2"></i> -->
 			<span class="d-none d-lg-inline">Charity Profile</span>
 		</NavLink>
 		</li>
 		<li class="nav-item dropdown no-arrow ms-1 osahan-profile-dropdown">
 		<a class="nav-link dropdown-toggle pe-0" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-			<img class="img-profile rounded-circle" src="img/p13.png">
+			<i class="fa fa-bars fa-lg"></i>
 		</a>
 		<div class="dropdown-menu dropdown-menu-end shadow-sm">
 			<NavLink class="dropdown-item btn btn-link" href="/charity/logs">
-				<i class="feather-log-out me-1"></i>
+				<i class="far fa-receipt me-1"></i>
 				Logs
 			</NavLink>
-			<NavLink class="dropdown-item btn btn-link" href="/logout" 
-				method="post" as="button">
-				<i class="feather-log-out me-1"></i>
-				<span class="d-none d-lg-inline">Logout</span>
+			<NavLink class="dropdown-item btn btn-link" href="/logout" method="post" as="button">
+				<i class="fas fa-sign-out me-1"></i>
+				Logout
 			</NavLink>
 		</div>
 	</li>
