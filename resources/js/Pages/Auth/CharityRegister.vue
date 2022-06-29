4d3ddfd19f6231983d7042c0ecabe2169623fcc5
Simon <simojoseph.pangan@gmail.com>
2022-06-29 22:04:58 +0800

    add head tag in charity register

--- resources/js/Pages/Auth/CharityRegister.vue
+++ resources/js/Pages/Auth/CharityRegister.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="container">
+  <Head title="Charity Register" />
+  <div class="container mb-4 mt-4">
     <form @submit.prevent="submit">
-
       <section v-if="step == 1">
         <div class="row justify-content-center align-items-center d-flex vh-100">
           <div class="col-md-4">
             <i class="feather-arrow-left me-2"></i>
-            <a class="fw-bold" href="sign-in.html">Return to Login</a>
+             <Link class="fw-bold" :href="$route('auth.login')">
+              <i class="far fa-angle-double-left fa-lg"></i>
+              Return to Login
+            </Link>
             <div class="osahan-login py-4">
               <div class="text-center mb-4">
                 <a href="index.html">
@@ -21,12 +24,9 @@
                     <label class="mb-1">Organization Name</label>
                     <div class="position-relative icon-form-control">
                       <i class="feather-user position-absolute"></i>
-                      <input v-model="form.charityName" type="text" class="form-control" />
-                      <p class="text-danger" v-if="v$.charityName.$error">
-                        <small>{{ v$.charityName.$errors[0].$message }}</small>
-                      </p>
-                      <div v-if="errors.charityName" class="text-danger">
-                        {{ errors.charityName }}
+                      <input v-model="form.name" type="text" class="form-control" />
+                      <div v-if="form.errors.name" class="text-danger">
+                        {{ form.errors.name }}
                       </div>
                     </div>
                   </div>
@@ -37,12 +37,9 @@
                   <label class="mb-1">Organization Email</label>
                   <div class="position-relative icon-form-control">
                     <i class="feather-at-sign position-absolute"></i>
-                    <input v-model.trim="form.charityEmail" type="email" class="form-control" />
-                    <p class="text-danger" v-if="v$.charityEmail.$error">
-                      <small>{{ v$.charityEmail.$errors[0].$message }}</small>
-                    </p>
-                    <div v-if="errors.charityEmail">
-                      {{ errors.charityEmail }}
+                    <input v-model.trim="form.charity_email" type="email" class="form-control" />
+                    <div v-if="errors.charity_email" class="text-danger">
+                      {{ errors.charity_email }}
                     </div>
                   </div>
                 </div>
@@ -52,12 +49,23 @@
                   <div class="form-group">
                     <label class="mb-1">Our Organization deals with</label>
                     <div class="position-relative icon-form-control">
-                      <select v-model="form.charityCategory" class="form-select" aria-label="Default select example">
-                        <option disabled selected>Please select one</option>
-                        <option  v-for="(category, index) in $page.props.charityCategories" :key="index" :value="category.name">
-                        {{ category.name}}
-                        </option>
-                      </select>
+                      <ul class="ks-cboxtags">
+                        <li v-for="(category, index) in charityCategories" :key="category.id">
+                          <input
+                            v-model="form.preferences"
+                            type="checkbox"
+                            :id="'checkbox' + index"
+                            :value="category.id"
+                          />
+                          <label :for="'checkbox' + index">{{ category.name }}</label>
+                        </li>
+                        <div v-if="form.errors['preferences.0']" class="text-danger">
+                          {{ form.errors['preferences.0'] }}
+                        </div>
+                        <div v-if="form.errors.preferences" class="text-danger">
+                          {{ form.errors['preferences'] }}
+                        </div>
+                      </ul>
                     </div>
                   </div>
                 </div>
@@ -68,17 +76,19 @@
                     <label class="mb-1">Head Admin - Email</label>
                     <div class="position-relative icon-form-control">
                       <i class="feather-at-sign position-absolute"></i>
-                      <input v-model.trim="form.headAdminEmail" type="email" class="form-control" />
-                      <p class="text-danger" v-if="v$.headAdminEmail.$error">
-                        <small>{{ v$.headAdminEmail.$errors[0].$message }}</small>
-                      </p>
-                      <div v-if="errors.headAdminEmail">{{ errors.headAdminEmail }}</div>
+                      <input v-model.trim="form.email" type="email" class="form-control" />
+                      <small class="form-text text-muted">
+                        This is the email you are going to use in login.
+                      </small>
+                      <div v-if="errors.email" class="text-danger">
+                        {{ errors.email }}
+                      </div>
                     </div>
                   </div>
                 </div>
                 <div class="row">
                   <div class="col-12">
-                    <label class="mb-1">Password (8 or more characters)</label>
+                    <label class="mb-1">Password</label>
                     <div class="position-relative icon-form-control">
                       <input v-model.trim="form.password" type="password" id="password" name="password" class="form-control" />
                       <div class="mt-2">
@@ -107,19 +117,16 @@
                           <p style="color: green" class="nmb-1" v-if="!v$.password.containsUppercase.$invalid"> &check; Passwords requires an upper case character. </p>
                         </small>
                         <small>
-                          <p style="color: orange" class="nmb-1" v-if="v$.password.containsUppercase.$invalid"> &#10006; Passwords requires an upper case character. </p>
-                        </small>
-                        <small>
                           <p style="color: orange" v-if="
                             v$.password.minLength.$invalid ||
-                            v$.password.maxLength.$invalid ||
                             form.password == ''
-                          "> &#10006; Passwords needs to be in between 8 to 19 characters. </p>
-                          <p style="color: green" v-else> &check; Passwords needs to be in between 8 to 19 characters. </p>
+                          "> &#10006; Passwords needs to have atlest 8 characters.</p>
+                          <p style="color: green" v-else> &check;  Passwords needs to have atlest 8 characters.</p>
                         </small>
-                        <small></small>
                       </div>
-                      <div v-if="errors.password">{{ errors.password }}</div>
+                      <div v-if="errors.password" class="text-danger">
+                        {{ errors.password }}
+                      </div>
                     </div>
                   </div>
                   <div class="col">
@@ -127,12 +134,6 @@
                     <div class="position-relative icon-form-control">
                       <i class="feather-unlock position-absolute"></i>
                       <input v-model.trim="form.password_confirmation" type="password" class="form-control" />
-                      <div class="text-danger" v-if="v$.password_confirmation.$error">
-                        <p class="text-danger">
-                          <small>{{ v$.password_confirmation.$errors[0].$message
-                        }}</small>
-                        </p>
-                      </div>
                     </div>
                   </div>
                 </div>
@@ -200,11 +201,8 @@
                     <div class="position-relative icon-form-control">
                       <i class="feather-user position-absolute"></i>
                       <textarea v-model="form.description" type="text" class="form-control" rows="6"></textarea>
-                      <p class="text-danger" v-if="v$.charityName.$error">
-                        <small>{{ v$.charityName.$errors[0].$message }}</small>
-                      </p>
-                      <div v-if="errors.charityName" class="text-danger">
-                        {{ errors.charityName }}
+                      <div v-if="errors.name" class="text-danger">
+                        {{ errors.name }}
                       </div>
                     </div>
                   </div>
@@ -216,11 +214,8 @@
                   <div class="position-relative icon-form-control">
                     <i class="feather-at-sign position-absolute"></i>
                     <input v-model.trim="form.fb_link" type="email" class="form-control" />
-                    <p class="text-danger" v-if="v$.charityEmail.$error">
-                      <small>{{ v$.charityEmail.$errors[0].$message }}</small>
-                    </p>
-                    <div v-if="errors.charityEmail">
-                      {{ errors.charityEmail }}
+                    <div v-if="errors.charity_email">
+                      {{ errors.charity_email }}
                     </div>
                   </div>
                 </div>
@@ -231,11 +226,8 @@
                   <div class="position-relative icon-form-control">
                     <i class="feather-at-sign position-absolute"></i>
                     <input v-model.trim="form.twitter_link" type="email" class="form-control" />
-                    <p class="text-danger" v-if="v$.charityEmail.$error">
-                      <small>{{ v$.charityEmail.$errors[0].$message }}</small>
-                    </p>
-                    <div v-if="errors.charityEmail">
-                      {{ errors.charityEmail }}
+                    <div v-if="errors.charity_email">
+                      {{ errors.charity_email }}
                     </div>
                   </div>
                 </div>
@@ -246,11 +238,8 @@
                   <div class="position-relative icon-form-control">
                     <i class="feather-at-sign position-absolute"></i>
                     <input v-model.trim="form.ig_link" type="email" class="form-control" />
-                    <p class="text-danger" v-if="v$.charityEmail.$error">
-                      <small>{{ v$.charityEmail.$errors[0].$message }}</small>
-                    </p>
-                    <div v-if="errors.charityEmail">
-                      {{ errors.charityEmail }}
+                    <div v-if="errors.charity_email">
+                      {{ errors.charity_email }}
                     </div>
                   </div>
                 </div>
@@ -261,17 +250,14 @@
                   <div class="position-relative icon-form-control">
                     <i class="feather-at-sign position-absolute"></i>
                     <input v-model.trim="form.website_link" type="email" class="form-control" />
-                    <p class="text-danger" v-if="v$.charityEmail.$error">
-                      <small>{{ v$.charityEmail.$errors[0].$message }}</small>
-                    </p>
-                    <div v-if="errors.charityEmail">
-                      {{ errors.charityEmail }}
+                    <div v-if="errors.charity_email">
+                      {{ errors.charity_email }}
                     </div>
                   </div>
                 </div>
               </div>
 
-                    <label class="pb-5">Organization Logo</label>
+                  <label class="pb-5">Organization Logo</label>
                     <div v-if="this.image_file">
                         <p>Uploaded File : </p>
                          <div class="d-flex mx-auto">
@@ -310,17 +296,11 @@
 
             </div>
           </div>
-          <div class="
-            col-md-4
-            justify-content-center
-            align-items-center
-            d-flex
-            vh-100
-          ">
+          <div class="col-md-4 justify-content-center align-items-center d-flex vh-100">
             <div class="card card-custom bg-white border-white border-0">
               <div class="card-custom-img" style="
-                  background-image: url(http://res.cloudinary.com/d3/image/upload/c_scale,q_auto:good,w_1110/trianglify-v1-cs85g_cc5d2i.jpg);
-                "></div>
+                background-image: url(http://res.cloudinary.com/d3/image/upload/c_scale,q_auto:good,w_1110/trianglify-v1-cs85g_cc5d2i.jpg);
+              "></div>
               <div class="card-body" style="overflow-y: auto">
                 <div class="wrapper">
                   <ul class="StepProgress">
@@ -441,44 +421,27 @@
   </div>
 </template>
 <script>
-  import {
-    computed
-  } from "vue";
-  import {
-    Inertia
-  } from "@inertiajs/inertia";
+  import { computed } from "vue";
+  import { Inertia } from "@inertiajs/inertia";
   import useVuelidate from "@vuelidate/core";
-  import {
-    helpers,
-    required,
-    minLength,
-    maxLength,
-    email,
-    sameAs,
-  } from "@vuelidate/validators";
-  import {
-    useForm
-  } from "@inertiajs/inertia-vue3";
-  // Import Vue FilePond
+  import { helpers, required, minLength, maxLength, email, sameAs } from "@vuelidate/validators";
+  import { useForm } from "@inertiajs/inertia-vue3";
   import vueFilePond from "vue-filepond";
-  // Import FilePond styles
   import "filepond/dist/filepond.min.css";
-  // Import image preview and file type validation plugins
   import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
   import FilePondPluginImagePreview from "filepond-plugin-image-preview";
 
-  // Create component
   const FilePond = vueFilePond(FilePondPluginFileValidateType,FilePondPluginImagePreview);
+
   export default {
     components: {
       FilePond,
     },
     setup() {
       let form = useForm({
-        charityName: null,
-        charityCategory: null,
-        charityEmail: null,
-        headAdminEmail: null,
+        name: null,
+        email: null,
+        charity_email: null,
         password: null,
         password_confirmation: null,
         description: null,
@@ -487,26 +450,14 @@
         ig_link: null,
         website_link: null,
         file: [],
+        categories: [],
         documentFile: null,
+        preferences: []
       });
+
       const rules = computed(() => ({
-        charityName: {
-          required: helpers.withMessage("Org Email is invalid", required),
-        },
-        charityCategory: {
-          required: helpers.withMessage("Please select at least 1 category", required),
-        },
-        charityEmail: {
-          required: helpers.withMessage("Org Email is invalid", required),
-          email,
-        },
-        headAdminEmail: {
-          required: helpers.withMessage("Head Admin Email is invalid", required),
-          email,
-        },
         password: {
-          minLength: minLength(9),
-          maxLength: maxLength(19),
+          minLength: minLength(8),
           required: helpers.withMessage("Password cannot be empty", required),
           containsSpecial: helpers.withMessage(
             () => `The password requires an special character`,
@@ -521,29 +472,20 @@
             () => `The password requires an uppercase character`,
             (value) => /[A-Z]/.test(value)),
         },
-        password_confirmation: {
-          required: helpers.withMessage("Confirm Password cannot be empty", required),
-          sameAs: helpers.withMessage("Confirm Password is incorrect", sameAs(form.password)),
-        },
-        city: {
-          required: helpers.withMessage("City cannot be empty", required)
-        },
       }));
+
       const v$ = useVuelidate(rules, form);
 
       function submit() {
         form.post(route("register.charity.store"))
       }
-      return {
-        form,
-        v$,
-        submit
-      };
+
+      return { form, v$, submit };
     },
     props: {
       errors: Object,
       csrfToken: String,
-    charityCategories: Array
+      charityCategories: Array
     },
     data() {
       return {
@@ -569,39 +511,23 @@
 
         this.form.file = file[0].file;
       },
-      validateFirstStepFields: function() {
-        this.v$.charityName.$validate();
-        this.v$.charityEmail.$validate();
-        this.v$.headAdminEmail.$validate();
-        this.v$.password.$validate();
-        this.v$.password_confirmation.$validate();
-      },
-      validateSecondStepFields: function() {},
       prevStep: function() {
         this.step--;
       },
       nextStep: function(e) {
-        //this.step++;
-        this.validateFirstStepFields();
-        //If field is correct
-        if (!this.v$.charityName.$error && !this.v$.charityCategory.$error && !this.v$.charityEmail.$error && !this.v$.headAdminEmail.$error && !this.v$.password_confirmation.$error) {
-          alert("Form ok");
-          this.step++;
-        } else alert("not okay");
+        this.form.post(route("register.charity.store", {
+          step: 1,
+        }), {
+          onSuccess: () => this.step++,
+        })
       },
       secondNextStep: function(e) {
         this.step++;
       },
-      thirdNextStep: function(e) {
-        this.v$.preferences.$validate();
-        if (!this.v$.preferences.$error) {
-          console.log(this.form.preferences);
-        } else alert("not okay");
-        console.log(this.form.preferences);
-      },
     },
   };
 </script>
+
 <style>
   @import "filepond/dist/filepond.css";
 
