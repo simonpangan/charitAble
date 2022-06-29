c1e164fad8d868b43fd8174f2deb1d56062265a6
Simon <simojoseph.pangan@gmail.com>
2022-06-29 21:54:45 +0800

    remove image in volunteer post

--- resources/js/Pages/Benefactor/Connections/Volunteer.vue
+++ resources/js/Pages/Benefactor/Connections/Volunteer.vue
@@ -21,7 +21,6 @@
                                 {{ post.location }}
                               </div>
                             </div>
-                            <img class="img-fluid ml-auto" src="img/l1.png" alt="">
                           </div>
                           <div class="p-3 job-item-footer">
                             <small class="text-gray-500">
