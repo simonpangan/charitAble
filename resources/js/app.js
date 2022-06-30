7f5f0bb5002818e5e56c1f1fc8483189d4da98e1
Simon <simojoseph.pangan@gmail.com>
2022-06-30 15:31:31 +0800

    add sweet alert to vue js

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,8 +4,11 @@ import "./icons";
 import { createApp, h } from 'vue'
 import { createInertiaApp, Link, Head } from '@inertiajs/inertia-vue3'
 import { InertiaProgress } from '@inertiajs/progress'
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 import Layout from './Shared/Layout.vue';
+
 
 createInertiaApp({
     resolve: async name => {
@@ -23,6 +26,7 @@ createInertiaApp({
         VueApp.config.globalProperties.$domain = window.domain;
 
         VueApp.use(plugin)
+            .use(VueSweetalert2)
             .component('Link', Link)
             .component('Head', Head)
             .mount(el);
