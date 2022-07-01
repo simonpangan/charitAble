c13d412c90cb9728abf50f22388b9c531c2bb266
Simon <simojoseph.pangan@gmail.com>
2022-07-01 23:07:54 +0800

    fix program create front-end

--- resources/js/Pages/Charity/Program/Create.vue
+++ resources/js/Pages/Charity/Program/Create.vue
@@ -17,9 +17,6 @@
             <div class="border rounded bg-white mb-3">
               <div class="box-title border-bottom p-3">
                 <h6 class="m-0">Program Image Header</h6>
-                <p class="mb-0 mt-0 small">
-                  Tell about yourself in two sentences.
-                </p>
               </div>
               <div class="box-body">
                 <div class="p-3 border-bottom">
@@ -68,10 +65,7 @@
           <main class="col-md-8">
             <div class="border rounded bg-white mb-3">
               <div class="box-title border-bottom p-3">
-                <h6 class="m-0">Edit Basic Info</h6>
-                <p class="mb-0 mt-0 small">
-                  Lorem ipsum dolor sit amet, consecteturs.
-                </p>
+                <h6 class="m-0">Program Basic Info</h6>
               </div>
               <div class="box-body p-3">
                 <form class="js-validate" novalidate="novalidate">
@@ -88,10 +82,6 @@
                             class="form-control"
                             v-model="form.name"
                           />
-                          <small class="form-text text-muted"
-                            >Displayed on your public profile, notifications and
-                            other places.</small
-                          >
                           <span v-if="form.errors.name" v-text="form.errors.name"
                             class="invalid-feedback d-block" role="alert">
                           </span>
@@ -131,7 +121,6 @@
                         </div>
                       </div>
                     </div>
-                    <!-- End Input -->
                   </div>
                 </form>
               </div>
@@ -139,81 +128,41 @@
             <div class="border rounded bg-white mb-3">
               <div class="box-title border-bottom p-3">
                 <h6 class="m-0">Goal</h6>
-                <p class="mb-0 mt-0 small">
-                  Tell about your work, job, and other experiences.
-                </p>
               </div>
               <div class="box-body px-3 pt-3 pb-0">
-                <div class="row">
+                <div class="row" v-for="(goal, index) in form.goals" :key="index">
                   <div class="col-sm-6 mb-4">
                     <label id="FROM" class="form-label">Goal</label>
                     <span class="text-danger">*</span>
-                    <div v-for="key in count" :key="key">
-                      <div class="input-group">
-                        <input
-                          type="text"
-                          class="form-control mb-2"
-                          v-model="goal_values[key]"
-                        />
-                        <span v-if="form.errors.location" v-text="form.errors.description"
-                            class="invalid-feedback d-block" role="alert">
-                        </span>
-                      </div>
-                    </div>
-                    <div class="d-flex">
-                      <a
-                        class="d-inline-block u-text-muted"
-                        href="#"
-                        @click="add"
-                      >
-                        <span class="mr-1">+</span>
-                        Add Goal
-                      </a>
-                      <a
-                        class="d-inline-block text-danger mx-auto"
-                        href="#"
-                        v-if="count > 1"
-                        @click="remove"
-                      >
-                        <span class="mr-1">+</span>
-                        Remove Goal
-                      </a>
+                    <div class="input-group">
+                        <input type="text" class="form-control mb-2" v-model="goal.name" />
                     </div>
                   </div>
                   <div class="col-sm-6 mb-4">
-                    <label id="TO" class="form-label"
-                      >Date of Expected Completion</label
-                    >
-                    <div v-for="key in count" :key="key">
-                      <div class="input-group">
-                        <input
-                          type="date"
-                          class="form-control mb-2"
-                          v-model="goal_date_values[key]"
-                          placeholder="From"
-                          aria-label="FROM"
-                          aria-describedby="FROM"
-                        />
-                        <span v-if="form.errors.goals" v-text="form.errors.description"
-                            class="invalid-feedback d-block" role="alert">
-                        </span>
-                      </div>
+                    <label class="form-label">Date of Expected Completion</label>
+                    <div class="input-group">
+                      <input type="date" class="form-control mb-2" v-model="goal.date"/>
+                        <button @click.stop.prevent="removeGoal(index)"
+                          class="ms-2 py-0 btn btn-sm btn-danger" style="height: 35px">
+                          <i class="fad fa-trash"></i>
+                      </button>
                     </div>
                   </div>
                 </div>
+                <button class="btn btn-light d-inline-block u-text-muted mb-2" 
+                    @click.stop.prevent="addGoal">
+                    <i class="fad fa-plus"></i>
+                    Add Goal
+                </button>
               </div>
             </div>
             <div class="border rounded bg-white mb-3">
               <div class="box-title border-bottom p-3">
                 <h6 class="m-0">Program Donation Details</h6>
-                <p class="mb-0 mt-0 small">
-                  Lorem ipsum dolor sit amet, consecteturs.
-                </p>
               </div>
               <div class="box-body p-3">
                 <form class="js-validate" novalidate="novalidate">
                   <div class="row">
-                    <!-- Input -->
                     <div class="col-sm-12 mb-2">
                       <div class="js-form-message">
                         <label id="nameLabel" class="form-label"> 
@@ -221,11 +170,7 @@
                           <span class="text-danger">*</span>
                         </label>
                         <div class="form-group">
-                          <input
-                            type="number"
-                            class="form-control"
-                            v-model="form.total_needed_amount"
-                          />
+                          <input type="number" class="form-control" v-model="form.total_needed_amount" />
                           <span v-if="form.errors.total_needed_amount" 
                             v-text="form.errors.total_needed_amount"
                             class="invalid-feedback d-block" role="alert">
@@ -234,68 +179,40 @@
                       </div>
                     </div>
                   </div>
-                  <div class="row">
+                  <div class="row" v-for="(expense, index) in form.expenses" :key="index">
                     <div class="col-sm-6 mb-4">
-                      <label id="FROM" class="form-label"
-                        >Add Program Expenses</label
-                      >
-                      <!-- Input -->
-                      <div v-for="keyProgram in countProgram" :key="keyProgram">
+                      <label id="FROM" class="form-label">Add Program Expenses</label>
                         <div class="input-group">
-                          <input
-                            type="text"
-                            class="form-control mb-2"
-                            v-model="expenses[keyProgram]"
-                          />
+                          <input type="text" class="form-control mb-2" v-model="expense.name" />
                         </div>
-                      </div>
-                      <div class="d-flex">
-                        <a
-                          class="d-inline-block u-text-muted"
-                          href="#"
-                          @click="addProgram"
-                        >
-                          <span class="mr-1">+</span>
-                          Add Program Expenses
-                        </a>
-                        <a
-                          class="d-inline-block text-danger mx-auto"
-                          href="#"
-                          v-if="countProgram > 1"
-                          @click="removeProgram"
-                        >
-                          <span class="mr-1">+</span>
-                          Add Program Expenses
-                        </a>
-                      </div>
                     </div>
                     <div class="col-sm-6 mb-4">
-                      <label id="TO" class="form-label"
-                        >Date of Expected Completion</label
-                      >
-                      <div v-for="keyProgram in countProgram" :key="keyProgram">
-                        <div class="input-group">
-                          <input
-                            type="number"
-                            class="form-control mb-2"
-                            v-model="program_date_expenses[keyProgram]"
-                          />
-                        </div>
+                      <label class="form-label">Date of Expected Completion</label>
+                      <div class="input-group">
+                        <input type="date" class="form-control mb-2" v-model="expense.amount" />
+                        <button @click.stop.prevent="removeExpense(index)"
+                          class="ms-2 py-0 btn btn-sm btn-danger" style="height: 35px">
+                          <i class="fad fa-trash"></i>
+                        </button>
                       </div>
                     </div>
                   </div>
+                  <button class="btn btn-light d-inline-block u-text-muted" 
+                    @click.stop.prevent="addExpense">
+                    <i class="fad fa-plus"></i>
+                    Add Program Expenses
+                  </button>
                 </form>
               </div>
             </div>
-            <div class="mb-3 text-right">
-              <a class="font-weight-bold btn btn-link rounded p-3">
-                &nbsp;&nbsp;&nbsp;&nbsp; Cancel &nbsp;&nbsp;&nbsp;&nbsp;
-              </a>
-              <button
-                class="font-weight-bold btn btn-primary rounded p-3"
-                @click="SubmitFormButton"
-              >
+            <div class="mb-3 text-right float-end">
+              <Link class="btn btn-danger rounded p-3 me-3" v-on:click="goBack">
+                <i class="far fa-arrow-left me-2"></i>
+                Cancel
+              </Link>
+              <button type="submit" class="font-weight-bold btn btn-primary rounded p-3">
                 Save Changes
+                 <i class="far fa-arrow-right me-2"></i>
               </button>
             </div>
           </main>
@@ -306,22 +223,18 @@
 </template>
 
 <script>
-import { reactive } from "vue";
-import { Inertia } from "@inertiajs/inertia";
 import { useForm } from "@inertiajs/inertia-vue3";
 
 import vueFilePond from "vue-filepond";
-// Import FilePond styles
 import "filepond/dist/filepond.min.css";
-// Import image preview and file type validation plugins
 import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
 import FilePondPluginImagePreview from "filepond-plugin-image-preview";
 
-// Create component
 const FilePond = vueFilePond(
   FilePondPluginFileValidateType,
   FilePondPluginImagePreview
 );
+
 export default {
   components: {
     FilePond,
@@ -331,8 +244,8 @@ export default {
       name: null,
       description: null,
       location: null,
-      goal: null,
-      expenses: null,
+      goals: [{name: '', date: ''}],
+      expenses: [{name: '', amount: ''}],
       header: [],
       total_needed_amount: null,
     });
@@ -346,69 +259,35 @@ export default {
   props: {
     csrfToken: String,
   },
-  data() {
-    return {
-      goalData: {},
-      count: 1,
-      countProgram: 1,
-      goal_values: [],
-      goal_date_values: [],
-      expenses: [],
-      program_date_expenses: [],
-      total_needed_amount: null,
-    };
-  },
   methods: {
     goBack() {
       return window.history.back();
     },
-    add: function () {
-      this.count++;
-   
+    addExpense(){
+      this.form.expenses.push({
+        name:'',
+        amount: ''
+      })
     },
-    remove: function () {
-      this.count--;
+    removeExpense(item){
+      this.form.expenses.splice(item, 1);
     },
-    addProgram: function () {
-      this.countProgram++;
+    addGoal(){
+      this.form.goals.push({
+        name:'',
+        date: ''
+      })
     },
-    removeProgram: function () {
-      this.countProgram--;
+    removeGoal(item){
+      this.form.goals.splice(item, 1);
     },
     handleFilePondUpdateFiles: function (file) {
       this.form.header = file[0].file;
     },
-    SubmitFormButton: function () {
-      console.log(this.goal_values + this.goal_date_values);
-      var goal_array = [];
-      var expenses = [];
-
-      for (let i = 0; i < this.goal_values.length; i++) {
-        if (this.goal_values[i] && this.goal_date_values[i] != null) {
-          //goal_array.push(this.goal_values[i] +','+ this.goal_date_values[i]);
-          goal_array.push({
-            goal: this.goal_values[i],
-            goal_date: this.goal_date_values[i],
-          });
-
-          this.form.goal = goal_array;
-        }
-      }
-
-      for (let i = 0; i < this.expenses.length; i++) {
-        if (this.expenses[i] && this.program_date_expenses[i] != null) {
-          expenses.push({
-            expenses: this.expenses[i],
-            program_date_expenses: this.program_date_expenses[i],
-          });
-          this.form.expenses = expenses;
-        }
-      }
-      this.form.post(route("charity.program.store"));
-    },
   },
 };
 </script>
+
 <style>
 @import "filepond/dist/filepond.css";
 
