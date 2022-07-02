93d00faecbeab7d90d6804833a91cf650d617ca0
Simon <simojoseph.pangan@gmail.com>
2022-07-02 19:45:47 +0800

    add icon

--- resources/js/Pages/Charity/Post/Create.vue
+++ resources/js/Pages/Charity/Post/Create.vue
@@ -8,32 +8,17 @@
           Go Back
         </Link>
         <form @submit.prevent="submit">
-          <div
-            class="box shadow-sm border rounded bg-white mb-3 mt-3 osahan-post"
-          >
-            <div
-              class="
-                p-3
-                d-flex
-                align-items-center
-                border-bottom
-                osahan-post-header
-              "
-            >
+          <div class="box shadow-sm border rounded bg-white mb-3 mt-3 osahan-post">
+            <div class="p-3 d-flex align-items-center border-bottom osahan-post-header">
               <div class="me-3">
                 <h6>Create Post</h6>
               </div>
             </div>
             <div class="p-3 border-bottom osahan-post-body">
               <div class="w-100">
-                <textarea
-                  placeholder="Write a message…"
-                  v-model="form.main_content_body"
-                  class="form-control border-0 p-3 shadow-none"
-                  rows="7"
-                ></textarea>
+                <textarea placeholder="Write a message…" v-model="form.main_content_body" class="form-control border-0 p-3 shadow-none" rows="7">
+                </textarea>
               </div>
-
               <file-pond
                 name="main_content_body_image"
                 class="h-50 mt-4 mb-2"
@@ -66,12 +51,9 @@
             <div class="p-3 border-bottom osahan-post-footer">
               <div class="p-3 d-flex">
                 <span class="mx-auto">
-                  <button
-                    :disabled="form.processing"
-                    type="submit"
-                    class="btn btn-primary btn-lg rounded"
-                  >
-                    <i class="feather-send"></i> Post !
+                  <button :disabled="form.processing" type="submit" class="btn btn-primary btn-lg rounded">
+                    Post
+                    <i class="fad fa-share ms-1"></i>
                   </button>
                 </span>
               </div>
