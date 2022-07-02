32f04bafb2df44c194e330e1d1ee744a7ce1559b
kevinxu101 <kevin.xu@benilde.edu.ph>
2022-07-02 23:21:41 +0800

    Changed link to button, fixed content

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -3,7 +3,7 @@
 
     <div class="container">
         <br />
-        <div class="row justify-content-center align-items-center d-flex">
+        <div class="row justify-content-center align-items-center d-flex vh-100">
             <div class="col-md-4 mx-auto">
                 <div class="py-4" :class="{ 'osahan-login': ! form.hasErrors }">
                     <div class="text-center mb-4">
@@ -16,14 +16,14 @@
                                 <i class="far fa-at position-absolute mt-2 ms-3"></i>
                                 <input  v-model="form.email" type="text"
                                     class="form-control" id="email"
-                                    :class="[{'is-invalid': props.errors.google_login}, 
+                                    :class="[{'is-invalid': props.errors.google_login},
                                         {'is-invalid': form.errors.email}]"
-                                />  
+                                />
                             </div>
                             <span v-if="form.errors.email" v-text="form.errors.email"
                                 class="invalid-feedback d-block" role="alert">
                             </span>
-                            <span v-if="props.errors.google_login" 
+                            <span v-if="props.errors.google_login"
                                     v-text="props.errors.google_login"
                                 class="invalid-feedback d-block" role="alert">
                             </span>
@@ -46,38 +46,43 @@
                             </label>
                         </div>
                         <br />
-                        <button :disabled="form.processing" type="submit" 
-                                class="btn btn-primary btn-block text-uppercase w-100"> 
-                            Sign in 
+                        <button :disabled="form.processing" type="submit"
+                                class="btn btn-primary btn-block text-uppercase w-100">
+                            Sign in
                             <i class="fal fa-sign-in"></i>
                         </button>
                         <div class="text-center mt-3 border-bottom pb-3">
                             <p class="small text-muted">Or</p>
                             <div class="text-center">
-                                <a :href="$route('auth.google.index')" 
+                                <a :href="$route('auth.google.index')"
                                     class="btn btn-light text-uppercase w-100">
                                     <i class="fab fa-google me-1"></i>
                                     Sign in with google
                                 </a>
                             </div>
                         </div>
-                        <div class="py-3 d-flex align-item-center">
+                        <div class="py-3 align-item-center mb-3">
+                        <div class="text-center">
+                            <div class="text-center">
+                                <a :href="$route('register.benefactor.index')"
+                                    class="btn btn-light text-uppercase w-100">
+                                   Sign up as Benefactor
+                                </a>
+                            </div>
+                        </div>
+
                             <Link :href="$route('auth.password.request')">
                                 Forgot password?
                             </Link>
-                            <div class="ms-auto">
-                                New to charitAble? 
-                                Join now as 
+
+                            <div class="ms-auto mt-3">
+                                Are you an NGO or a Charity? Let's work 
                                 <Link class="fw-bold" :href="$route('register.charity.index')">
-                                    Charity
-                                </Link>
-                                or
-                                <Link class="fw-bold" :href="$route('register.benefactor.index')">
-                                    Benefactor
+                                    together.
                                 </Link>
                             </div>
                         </div>
-                    </form> 
+                    </form>
                 </div>
             </div>
         </div>
