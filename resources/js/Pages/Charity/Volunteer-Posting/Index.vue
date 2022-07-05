2a906fa7228d4c8a3e1941d4649a00161b5f3484
Simon <simojoseph.pangan@gmail.com>
2022-07-05 10:47:43 +0800

    add note when there is norrucent volunteer posting

--- resources/js/Pages/Charity/Volunteer-Posting/Index.vue
+++ resources/js/Pages/Charity/Volunteer-Posting/Index.vue
@@ -6,30 +6,39 @@
                 <div class="row">
                     <div class="col-md-12">
                         <div class="row">
-                            <div v-for="volunteer_post in volunteerPost" :key="volunteer_post.id" class="col-md-6">
-                            <Link :href="$route('charity.volunteer.show', {
-                                'id': volunteer_post.id
-                            })">
-                                <div class="border job-item mb-3">
-                                    <div class="d-flex align-items-center p-3 job-item-header">
-                                        <div class="overflow-hidden me-2">
-                                            <h6 class="font-weight-bold text-dark mb-0 text-truncate">{{volunteer_post.name}}</h6>
-                                            <div class="text-truncate text-primary">{{this.$page.props.charity.name}}</div>
-                                            <div class="small text-gray-500">
-                                                <i class="fal fa-map-pin"></i>
-                                                {{volunteer_post.location}}
+                            <template v-if="volunteerPost.length > 0">
+                             <div v-for="volunteer_post in volunteerPost" :key="volunteer_post.id" class="col-md-6">
+                                <Link :href="$route('charity.volunteer.show', {
+                                    'id': volunteer_post.id
+                                })">
+                                    <div class="border job-item mb-3">
+                                        <div class="d-flex align-items-center p-3 job-item-header">
+                                            <div class="overflow-hidden me-2">
+                                                <h6 class="font-weight-bold text-dark mb-0 text-truncate">{{volunteer_post.name}}</h6>
+                                                <div class="text-truncate text-primary">{{this.$page.props.charity.name}}</div>
+                                                <div class="small text-gray-500">
+                                                    <i class="fal fa-map-pin"></i>
+                                                    {{volunteer_post.location}}
+                                                </div>
                                             </div>
                                         </div>
+                                        <div class="p-3 job-item-footer">
+                                            <small class="text-gray-500">
+                                                <i class="fas fa-clock"></i>
+                                                {{volunteer_post.created_at_formatted}}
+                                            </small>
+                                        </div>
                                     </div>
-                                    <div class="p-3 job-item-footer">
-                                        <small class="text-gray-500">
-                                            <i class="fas fa-clock"></i>
-                                            {{volunteer_post.created_at_formatted}}
-                                        </small>
-                                    </div>
-                                </div>
-                            </Link>
-                        </div>
+                                </Link>
+                            </div>
+                            </template>
+                             <div v-else>
+                                <figure class="mb-0 text-center">
+                                    <blockquote class="blockquote">
+                                        <p>The charity has no current volunteer posting.</p>
+                                    </blockquote>
+                                </figure>
+                            </div>
                         </div>
                     </div>
                 </div>
