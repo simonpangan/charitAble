b4fbbbe7de740af876e283de70b9c3e8f9a5535d
Simon <simojoseph.pangan@gmail.com>
2022-07-05 15:05:32 +0800

    add note when there is no supporter yet

--- resources/js/Pages/Charity/Program/Supports.vue
+++ resources/js/Pages/Charity/Program/Supports.vue
@@ -4,28 +4,39 @@
 		 <div class="tab-pane fade show active" id="contact" role="tabpanel" aria-labelledby="contact-tab">
 			<div class="box shadow-sm border rounded bg-white p-3">
 				<div class="row">
-					<div v-for="supporter in program.supporters" :key="supporter.id" class="col-md-3">
-						<div class="border job-item mb-3">
-							<div class="d-flex align-items-center p-3 job-item-header">
-								<div class="overflow-hidden me-2">
-									<h6 class="font-weight-bold text-dark mb-0 text-truncate">
-										<span class="fa-1x text-gray-300">₱</span>
-										{{ supporter.amount }}
-									</h6>
-									<div class="small text-gray-500">
-										{{ supporter.benefactor.first_name  + 
-											' ' + 
-											supporter.benefactor.last_name
-										}}
+					<template v-if="program.supporters.length != 0">
+						<div v-for="supporter in program.supporters" :key="supporter.id" class="col-md-3">
+							<div class="border job-item mb-3">
+								<div class="d-flex align-items-center p-3 job-item-header">
+									<div class="overflow-hidden me-2">
+										<h6 class="font-weight-bold text-dark mb-0 text-truncate">
+											<span class="fa-1x text-gray-300">₱</span>
+											{{ supporter.amount }}
+										</h6>
+										<div class="small text-gray-500">
+											{{ supporter.benefactor.first_name  + 
+												' ' + 
+												supporter.benefactor.last_name
+											}}
+										</div>
 									</div>
 								</div>
+								<div class="p-3 job-item-footer">
+									<small class="text-gray-500">
+										<i class="fas fa-clock"></i>
+										Donated at: {{ supporter.donated_at_formatted }}
+									</small>
+								</div>
 							</div>
-							<div class="p-3 job-item-footer">
-								<small class="text-gray-500">
-									<i class="fas fa-clock"></i>
-									Donated at: {{ supporter.donated_at_formatted }}
-								</small>
-							</div>
+						</div>
+					</template>
+					<div v-else>
+						<div class="card p-3">
+						<figure class="p-3 mb-0 text-center">
+							<blockquote class="blockquote">
+							<p>Program has no supporter yet.</p>
+							</blockquote>
+						</figure>
 						</div>
 					</div>
 				</div>
