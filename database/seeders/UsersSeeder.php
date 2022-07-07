7c69a78c74a0be1f3a95f6d6a0dfbbec86ead9c9
Simon <simojoseph.pangan@gmail.com>
2022-07-08 01:20:53 +0800

    change location of charity seeder

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -73,7 +73,7 @@ class UsersSeeder extends Seeder
                 'role_id' => Role::USERS['CHARITY_SUPER_ADMIN'],
                 'email_verified_at' => Carbon::now(config('app.timezone')),
                 'address' => "1012 Sta. Maria St., Malinta",
-                'location_id' => 1,
+                'location_id' => 6,
                 'password' => Hash::make('charity'),
                 'created_at' => '2021-06-18 01:15:48',
             ]
