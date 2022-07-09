681b2a996e23eb4cc721907ee73a28f6957d749f
Simon <simojoseph.pangan@gmail.com>
2022-07-09 23:37:01 +0800

    add logo to email tmeplate

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,11 @@
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+@if (trim($slot) === 'Laravel')
+<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+@else
+<img src="{{ asset('logo/with-name.png') }}" class="logo" alt="Charitable Logo">
+@endif
+</a>
+</td>
+</tr>
