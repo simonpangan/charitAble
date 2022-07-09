4202cd2b1601f7658cb47991d330263a2c27268f
Simon <simojoseph.pangan@gmail.com>
2022-07-09 18:37:20 +0800

    update email to include transaction

--- resources/views/mail/withdraw-request.blade.php
+++ resources/views/mail/withdraw-request.blade.php
@@ -5,7 +5,12 @@
     {{ $program->withdraw_request_amount }} 
     to their program {{ $program->name }}
 </p> 
-
+<p>
+    <b>Transaction Hash: </b>
+    <a target="_blank" href="https://rinkeby.etherscan.io/tx/{{ $transaction_hash }}">
+        {{ $transaction_hash }}
+    </a>
+</p>
 Thanks,<br>
 {{ config('app.name') }}<br>
 @endcomponent
