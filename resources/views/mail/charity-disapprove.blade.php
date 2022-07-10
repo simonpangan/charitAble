97faf4d4897e157f919a730497627f240cdaee1e
Simon <simojoseph.pangan@gmail.com>
2022-07-10 12:43:50 +0800

    add disapprove email

---
+++ resources/views/mail/charity-disapprove.blade.php
@@ -0,0 +1,15 @@
+@component('mail::message')
+<h2>Good Day {{ $charity->name }},</h2>
+<p>
+    We are pleased to inform you that your organization has been declined for verification. 
+</p> 
+    
+<p><b>Reason: </b>{{ $message }}</p>
+
+<p>
+    If you have any questions feel free to ask.
+</p> 
+
+Thanks,<br>
+{{ config('app.name') }}<br>
+@endcomponent
