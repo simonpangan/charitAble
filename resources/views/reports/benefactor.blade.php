bb008be57444f6b3ce6da14a87befff0c7d6c856
Simon <simojoseph.pangan@gmail.com>
2022-07-11 21:59:18 +0800

    add header and footer to report

--- resources/views/reports/benefactor.blade.php
+++ resources/views/reports/benefactor.blade.php
@@ -7,14 +7,62 @@
         .page-break {
             page-break-after: always;
         }
-    
-    </style>
+        /** 
+            Set the margins of the page to 0, so the footer and the header
+            can be of the full height and width !
+            **/
+        @page {
+            margin: 0cm 0cm;
+        }
+
+        /** Define now the real margins of every page in the PDF **/
+        body {
+            margin-top: 2cm;
+            margin-left: 2cm;
+            margin-right: 2cm;
+            margin-bottom: 2cm;
+        }
+
+        /** Define the header rules **/
+        header {
+            position: fixed;
+            top: 0cm;
+            left: 0cm;
+            right: 0cm;
+            height: 2cm;
+
+            margin-bottom: 2rem;
+                
+            text-align: center;
+            line-height: 1.5cm;
+        }
+
+        /** Define the footer rules **/
+        footer {
+            position: fixed; 
+            bottom: 0cm; 
+            left: 0cm; 
+            right: 0cm;
+            height: 2cm;
+
+            /** Extra personal styles **/
+            background-color: #03a9f4;
+            color: white;
+            text-align: center;
+            line-height: 1.5cm;
+        }
+    </style>    
         
 </head>
 <body>
-    {{-- <header>
-        <img src="{{ asset('logo/logo-only.png') }}" width="100%" height="100%"/>
-    </header> --}}
+    <header>
+        <img src="{{ public_path('logo/with-name.jpg') }}" width="75px" height="75px"/>
+    </header>
+
+    <footer>
+        Copyright &copy; <?php echo date("Y");?> 
+    </footer>
+
 
     <h1 class="text-center">{{ $title }}</h1>
     <p>Date: {{ $date }}</p>
