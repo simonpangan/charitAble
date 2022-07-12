7e1442c32ea07f4c06761a7a2a78be0a43ec8c75
Simon <simojoseph.pangan@gmail.com>
2022-07-12 18:38:57 +0800

    add image link

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -4,7 +4,9 @@
 @if (trim($slot) === 'Laravel')
 <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
 @else
-<img src="{{ asset('logo/with-name.png') }}" class="logo" alt="Charitable Logo">
+<img src="https://scontent.xx.fbcdn.net/v/t1.15752-9/290346997_2042338865960910_8584169836172028681_n.png?_nc_cat=101&ccb=1-7&_nc_sid=aee45a&_nc_ohc=TbL8Sb03Zv8AX9Hl-28&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AVIH4Mo7Vgcsq9aqUPJWxFtjTY05OdlG5r7ZMKdnMIaJCw&oe=62F1E814" 
+	class="logo" alt="Charitable Logo"
+>
 @endif
 </a>
 </td>
