8f4a31e39e45abb4cad8a05fa261440f4ce6140d
Simon <simojoseph.pangan@gmail.com>
2022-07-14 17:31:32 +0800

    update logo  and favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,10 @@
     <link href="{{ mix('/css/vendor.css') }}" rel="stylesheet" />
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     @inertiaHead
-    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}"  type='image/x-icon'>
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('favicon/site.webmanifest') }}">
   </head>
   <body>
 
