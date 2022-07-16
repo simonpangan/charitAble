e678dca7dbb4609169885ac708ed2f5c58d0c371
kevinxu101 <kevin.xu@benilde.edu.ph>
2022-07-16 20:26:52 +0800

    Added Updates and Program Changes on Program

--- resources/js/Pages/Charity/Program/Updates.vue
+++ resources/js/Pages/Charity/Program/Updates.vue
@@ -7,28 +7,49 @@
       role="tabpanel"
       aria-labelledby="home-tab"
     >
-                <div  v-for="post in posts" :key="post.id">
-                    <div class="box shadow-sm border rounded bg-white mb-3 osahan-post">
-                        <div class="p-3 d-flex align-items-center border-bottom osahan-post-header">
-                            <div class="dropdown-list-image me-3">
-                                <img class="rounded-circle" v-bind:src="charity[0].logo"  alt="">
-                            </div>
-                            <div class="font-weight-bold">
-                                <div class="text-truncate">{{charity[0].name}}<span class="text-info ms-1"><i data-bs-toggle="tooltip" data-bs-placement="top" title="Verified" class="feather-check-circle"></i></span></div>
-                                <div class="small text-gray-500"> {{post.created_at_formatted}}</div>
-                            </div>
-                            <span class="ms-auto small"></span>
-                           
-                        </div>
-                        <div class="p-3 border-bottom osahan-post-body">
-                            {{post.main_content_body}}
-                            <img v-if="post.main_content_body_image"
-                                v-bind:src="post.main_content_body_image"
-                                class="img-fluid"  alt="post image"
-                            >
-                        </div>
-                    </div>
-                </div>
+      <div v-for="post in posts" :key="post.id">
+        <div class="box shadow-sm border rounded bg-white mb-3 osahan-post">
+          <div
+            class="
+              p-3
+              d-flex
+              align-items-center
+              border-bottom
+              osahan-post-header
+            "
+          >
+            <div class="dropdown-list-image me-3">
+              <img class="rounded-circle" v-bind:src="charity[0].logo" alt="" />
+            </div>
+            <div class="font-weight-bold">
+              <div class="text-truncate">
+                {{ charity[0].name
+                }}<span class="text-info ms-1"
+                  ><i
+                    data-bs-toggle="tooltip"
+                    data-bs-placement="top"
+                    title="Verified"
+                    class="feather-check-circle"
+                  ></i
+                ></span>
+              </div>
+              <div class="small text-gray-500">
+                {{ post.created_at_formatted }}
+              </div>
+            </div>
+            <span class="ms-auto small"></span>
+          </div>
+          <div class="p-3 border-bottom osahan-post-body">
+            {{ post.main_content_body }}
+            <img
+              v-if="post.main_content_body_image"
+              v-bind:src="post.main_content_body_image"
+              class="img-fluid"
+              alt="post image"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   </program-layout>
 </template>
@@ -39,7 +60,7 @@ import ProgramLayout from "./ProgramLayout";
 let props = defineProps({
   program: Array,
   posts: Array,
-  charity:Array
+  charity: Array,
 });
 </script>
 
