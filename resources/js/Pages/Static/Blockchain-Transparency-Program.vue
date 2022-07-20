ae65ae17387e174ab61bbf6f3addae4c47b7422e
kevinxu101 <kevin.xu@benilde.edu.ph>
2022-07-20 22:07:38 +0800

    Blockchain file static

---
+++ resources/js/Pages/Static/Blockchain-Transparency-Program.vue
@@ -0,0 +1,81 @@
+
+<template>
+	<Head title="Blockchain-Transparency-Program" />
+<div class="py-5 bg-primary">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-8 mx-auto">
+                  <h1 class="text-white font-weight-light"><span class="font-weight-bold">CharitAble Transparency</span> Program</h1>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="py-5">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-8 mx-auto">
+                  <div class="bg-white p-5 shadow-sm box border rounded">
+                     <div id="intro" class="mb-4">
+                        <!-- Title -->
+                        <div class="mb-3">
+                           <h2 class="h5 text-primary">How do we promote transparency?</h2>
+                        </div>
+                        <!-- End Title -->
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget porta ex. Nullam luctus nulla et venenatis laoreet. Mauris non vestibulum odio</p>
+                        <p>sit amet lobortis mauris. Suspendisse potenti. Nam iaculis dui mattis egestas cursus. Pellentesque habitant morbi tristique senectus et netus et
+                            malesuada fames ac turpis egestas. Integer nisl erat, ullamcorper eget dolor eu, congue viverra ligula.
+                            Quisque quam nisi, interdum quis dui sit amet, sodales venenatis elit. Donec ac ante nulla.
+                            Praesent id sem vitae dui aliquet porta. Morbi semper ipsum ut varius faucibus.
+                            Ut vehicula tellus quis dapibus congue. Orci varius natoque penatibus et magnis
+                            dis parturient montes, nascetur ridiculus mus. Morbi pellentesque blandit bibendum.
+                            Phasellus gravida, eros sed fringilla consectetur, sapien turpis cursus mauris, pulvinar dictum libero leo quis dolor.</p>
+                        <p>Our Services are very diverse, so sometimes additional terms or product requirements (including age requirements) may apply. Additional terms will be available with the relevant Services, and those additional terms become part of your agreement with us if you use those Services.</p>
+                     </div>
+                     <div id="services" class="mb-4">
+                        <!-- Title -->
+                        <div class="mb-3">
+                           <h3 class="h5 text-primary">How does our blockchain work?</h3>
+                        </div>
+                        <!-- End Title -->
+                         <p>sit amet lobortis mauris. Suspendisse potenti. Nam iaculis dui mattis egestas cursus. Pellentesque habitant morbi tristique senectus et netus et
+                            malesuada fames ac turpis egestas. Integer nisl erat, ullamcorper eget dolor eu, congue viverra ligula.
+                            Quisque quam nisi, interdum quis dui sit amet, sodales venenatis elit. Donec ac ante nulla.
+                            Praesent id sem vitae dui aliquet porta. Morbi semper ipsum ut varius faucibus.
+                            Ut vehicula tellus quis dapibus congue. Orci varius natoque penatibus et magnis
+                            dis parturient montes, nascetur ridiculus mus. Morbi pellentesque blandit bibendum.
+                            Phasellus gravida, eros sed fringilla consectetur, sapien turpis cursus mauris, pulvinar dictum libero leo quis dolor.</p>
+                        <!-- Title -->
+                        <img src="https://scontent.fcrk3-1.fna.fbcdn.net/v/t1.15752-9/291787429_589496739438600_8655618560543443343_n.png?_nc_cat=106&ccb=1-7&_nc_sid=ae9488&_nc_eui2=AeGqpwHAfYQU2KuoVtv4OuRCDWhuwZGNW60NaG7BkY1brdzR55-sOZ8jfztri95jjTUqDypQnEsrH38ha0dYjbYa&_nc_ohc=HGfYEnbDuXQAX8wS4DM&_nc_ht=scontent.fcrk3-1.fna&oh=03_AVJv4wsgextvh_M2ZTCa1b_DwIoCaHtrpBF6cWduyCGFig&oe=62FD5D27" class="img-fluid"/>
+                        <div id="personal-data" class="mb-3 active">
+                           <h4 class="h6 text-primary">A. Sample Blockchain Transaction</h4>
+                        </div>
+                        <!-- End Title -->
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget porta ex. Nullam luctus nulla et venenatis laoreet. Mauris non vestibulum odio</p>
+                        <ul class="text-secondary">
+                           <li class="pb-2"> 1. Indicates the status of blockchain transaction.</li>
+                           <li class="pb-2"> 2. Timestamp of the donation</li>
+                           <li class="pb-2"> 3. CharitAble Master Address (Benefactor Address)</li>
+                           <li class="pb-2"> 4. CharitAble Master Address (Benefactor Address)</li>
+                           <li class="pb-2"> 5. The Address of the Charity(Dynamic). Every charity has their own address upon verification.</li>
+                           <li class="pb-2"> 6. Donation Amount</li>
+
+                        </ul>
+                         <p>sit amet lobortis mauris. Suspendisse potenti. Nam iaculis dui mattis egestas cursus. Pellentesque habitant morbi tristique senectus et netus et
+                            malesuada fames ac turpis egestas. Integer nisl erat, ullamcorper eget dolor eu, congue viverra ligula.
+                            Quisque quam nisi, interdum quis dui sit amet, sodales venenatis elit. Donec ac ante nulla.
+                            Praesent id sem vitae dui aliquet porta. Morbi semper ipsum ut varius faucibus.
+                            Ut vehicula tellus quis dapibus congue. Orci varius natoque penatibus et magnis
+                            dis parturient montes, nascetur ridiculus mus. Morbi pellentesque blandit bibendum.
+                            Phasellus gravida, eros sed fringilla consectetur, sapien turpis cursus mauris, pulvinar dictum libero leo quis dolor.</p>                        <!-- Title -->
+
+                        <!-- End Title -->
+
+                     </div>
+
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+</template>
+
